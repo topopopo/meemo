db1f87d7581ed71dd0d3fb0eb5d2620907fdd7e2
takeaki ishizuka <takewatage.meily@gmail.com>
2018-08-13 01:12:18 +0900

    editor コンポーネント追加

---
+++ src/pages/Editor.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <h2>welcome!!!!</h2>
+        <br>
+        <div>{{name}}</div>
+        <br>
+        <button @click="signOut">log out</button>
+    </div>
+</template>
+
+<script>
+import firebase from 'firebase'
+
+export default {
+  name: 'editor',
+
+  data: () => ({
+    name: firebase.auth().currentUser.email
+  }),
+  methods: {
+    signOut: function () {
+      firebase.auth().signOut().then(() => {
+        this.$router.push('/')
+      })
+      firebase.auth().signOut()
+    }
+  },
+  computed: {
+  },
+  async mounted () {
+    // this.name = firebase.auth().currentUser.email
+  },
+  created () {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,24 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Welcome from '@/pages/Welcome'
+import Editor from '@/pages/Editor'
 
 Vue.use(Router)
 
-export default new Router({
+let router = new Router({
   routes: [
     {
       path: '/',
       name: 'welcmome',
-      component: Welcome
+      component: Welcome,
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/editor',
+      name: 'Editor',
+      component: Editor
     }
   ]
 })
+
+export default router
